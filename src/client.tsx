18d78d592ef216a44c34dc6677cc8410e375f3aa
Kasper Peulen <kasperpeulen@gmail.com>
2025-04-14 20:36:37 +0200

    Refactor

--- src/client.tsx
+++ src/client.tsx
@@ -1,14 +1,4 @@
-import { createRoot } from "react-dom/client";
+export { createRoot } from "react-dom/client";
 // @ts-ignore
-import { createFromReadableStream } from "react-server-dom-webpack/client";
-import { type JSX, use } from "react";
-
-function Root({ response }: { response: Promise<JSX.Element> }) {
-  return use(response);
-}
-
-export async function renderFlightStream(stream: ReadableStream) {
-  createRoot(document.getElementById("root")!).render(
-    <Root response={createFromReadableStream(stream)} />,
-  );
-}
+export { createFromReadableStream } from "react-server-dom-webpack/client";
+export { use } from "react";
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,22 @@
 // @ts-ignore
 import { renderToReadableStream } from "react-server-dom-webpack/server";
 import { Story } from "./components/users.stories";
+import type { JSX, Usable } from "react";
 
-async function main() {
-  const manifest = await fetch("/client/react-client-manifest.json").then(
-    (it) => it.json(),
-  );
-  const stream: ReadableStream = renderToReadableStream(<Story />, manifest);
-  const { renderFlightStream } = await import(
-    // @ts-ignore
-    /* webpackIgnore: true */ "/client/main.js"
-  );
-  await renderFlightStream(stream);
+const { use, createRoot, createFromReadableStream } = await import(
+  // @ts-ignore
+  /* webpackIgnore: true */ "/client/main.js"
+);
+const manifest = await fetch("/client/react-client-manifest.json").then((it) =>
+  it.json(),
+);
+
+function Use({ value }: { value: Usable<JSX.Element> }) {
+  return use(value);
 }
 
-main();
+const stream = renderToReadableStream(<Story />, manifest);
+
+createRoot(document.getElementById("root")!).render(
+  <Use value={createFromReadableStream(stream)} />,
+);
